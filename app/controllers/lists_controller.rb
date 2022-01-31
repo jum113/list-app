7178fa7af13be91707427d380052ad8ce6bade37
jum113 <japan_movieact@yahoo.co.jp>
2022-01-31 21:14:06 +0900

    リスト作成機能の実装

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,10 @@
 class ListsController < ApplicationController
+  def create
+    list = List.create(list_params)
+    render json: { post: list }
+  end
+
+  def list_params
+    params.require(:list).permit(:list_text).merge(user_id: current_user.id)
+  end
 end

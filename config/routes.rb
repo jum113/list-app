488150e333b91e44aa1d4a287110cccc98620a89
jum113 <japan_movieact@yahoo.co.jp>
2022-02-03 16:28:10 +0900

    アカウント編集ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'notes#index'
+  resources :users, only: [:edit, :update]
   resources :notes, only: [:index, :create, :destroy] do
     collection do
       get 'search'

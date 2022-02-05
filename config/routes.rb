df5a7900c5d46cd05410aee8ab815ab1d741861c
jum113 <japan_movieact@yahoo.co.jp>
2022-02-05 15:31:05 +0900

    ビューの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'notes#index'
-  resources :users, only: [:edit, :update]
   resources :notes, only: [:index, :create, :destroy] do
     collection do
       get 'search'
     end
   end
   resources :lists, only: [:create, :destroy]
+  resources :users, only: [:edit, :update]
 end
